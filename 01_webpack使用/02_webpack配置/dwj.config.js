48134df8e9967a9c326468f7d60b970ef0b1dfd8
dengwenj <927561237@qq.com>
2022-03-19 20:55:48 +0800

    webpack中使用postcss

--- 01_webpack使用/02_webpack配置/dwj.config.js
+++ 01_webpack使用/02_webpack配置/dwj.config.js
@@ -22,7 +22,17 @@ module.exports = {
           //   // options: {}
           // },
           'style-loader',
-          'css-loader' // 简写
+          'css-loader', // 简写
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  require('autoprefixer')
+                ]
+              }
+            }
+          }
         ]
       },
       {
