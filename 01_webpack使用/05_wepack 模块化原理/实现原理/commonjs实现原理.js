6cb8623c8f814fdd586648cce76c48f1c0d982b1
dengwenj <927561237@qq.com>
2022-03-20 22:52:46 +0800

    webpack打包后模块化的原理

---
+++ 01_webpack使用/05_wepack 模块化原理/实现原理/commonjs实现原理.js
@@ -0,0 +1,46 @@
+// webpack 打包过后的 commonjs模块化 源码
+var __webpack_modules__ = ({
+  "./src/js/common.js":
+    (function(module) {
+
+      function common1() {
+        return 'common1js'
+      }
+
+      function common2() {
+        return 'common2js'
+      }
+
+      module.exports = {
+        common1,
+        common2
+      }
+    })
+});
+
+var __webpack_module_cache__ = {};
+
+function __webpack_require__(moduleId) {
+
+  var cachedModule = __webpack_module_cache__[moduleId];
+  if (cachedModule !== undefined) {
+    return cachedModule.exports;
+  }
+
+  var module = __webpack_module_cache__[moduleId] = {
+    exports: {}
+  };
+
+  __webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+
+  return module.exports;
+}
+ 	
+var __webpack_exports__ = {};
+
+!function() {
+  const { common1, common2 } = __webpack_require__("./src/js/common.js")
+
+  console.log(common1())
+  console.log(common2())
+}();
