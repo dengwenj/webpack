309a184364eb428cd67dde8fa9758b1d783b23df
dengwenj <927561237@qq.com>
2022-03-20 21:21:53 +0800

    webpack打包过后的commonjs源码

---
+++ 01_webpack使用/05_wepack 模块化原理/dwj.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { DefinePlugin } = require('webpack')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  // 入口文件
+  // entry: './src/index.js',
+  entry: './src/common.js',
+  // 出口文件
+  output: {
+    filename: 'js/hh.js',
+    // 绝对路劲
+    path: path.resolve(__dirname, './dist'),
+    // assetModuleFilename: 'img/[name].[hash:6][ext]' // 这个统一都设置为 img 下面了
+  },
+  // plugins 配置
+  plugins: [
+    // 每次打包把上一次的打包删掉，不留任何的，重新打包
+    new CleanWebpackPlugin(),
+    // 在 打包的文件夹中增加个 html 文件
+    new HtmlWebpackPlugin({
+      title: 'hello webpack',
+      // template: './public/index.html', // 不要默认的 html 文件 自己写 html 文件
+    }),
+    // 允许在编译时创建配置的全局常量
+    new DefinePlugin({
+      BASE_URL: '"./"',
+      hh: '111111'
+    })
+  ]
+}
+ 
