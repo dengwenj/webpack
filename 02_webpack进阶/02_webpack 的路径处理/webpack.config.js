9f72efd5672c269f76391cbeb0c1cfcead3f97f0
dengwenj <927561237@qq.com>
2022-03-23 15:14:23 +0800

    publicPath的使用

---
+++ 02_webpack进阶/02_webpack 的路径处理/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path")
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader/dist/index')
+
+module.exports = {
+  mode: 'development',
+  // 监听源代码的变化
+  watch: true,
+  // 入口
+  entry: './src/index.js',
+  // 出口
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    // 在打包之后的静态资源前面进行一个路径的拼接 原来 bundle.js 现在 /bundle.js
+    publicPath: '/'
+  },
+  // 用来配置 dev-webpack-server 的
+  devServer: {
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.vue$/i,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html'
+    }),
+    new ReactRefreshWebpackPlugin(),
+    new VueLoaderPlugin()
+  ]
+}
