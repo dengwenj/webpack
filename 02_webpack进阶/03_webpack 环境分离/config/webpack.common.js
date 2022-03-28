57d6b20243aff30e99d9114e0d8ce444d0d31058
dengwenj <927561237@qq.com>
2022-03-28 18:26:47 +0800

    entry 写上相对路径，并不是相对于文件所在的路径，而是相对于 context 匹配的路径

--- 02_webpack进阶/03_webpack 环境分离/config/webpack.common.js
+++ 02_webpack进阶/03_webpack 环境分离/config/webpack.common.js
@@ -3,9 +3,11 @@ const path = require('path')
 module.exports = function (env) {
   console.log('env', env)
   const isProduction = env.production
-  console.log(isProduction)
 
   return {
+    // 绝对路径，用于从配置中解析入口点（entry point）和加载器（loader），默认就是写的 package.json 里面的那个来说的
+    context: path.resolve(__dirname, '../'),
+    // entry 写上相对路径，并不是相对于文件所在的路径，而是相对于 context 匹配的路径
     entry: './src/main.js',
     output: {
       path: path.resolve(__dirname, '../dist')
