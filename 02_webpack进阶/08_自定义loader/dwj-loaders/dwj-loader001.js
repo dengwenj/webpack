4a8e82711e4c5e4679afc25ac1bc1275f8b77c87
dengwenj <927561237@qq.com>
2022-04-01 16:39:25 +0800

    异步的loader

--- 02_webpack进阶/08_自定义loader/dwj-loaders/dwj-loader001.js
+++ 02_webpack进阶/08_自定义loader/dwj-loaders/dwj-loader001.js
@@ -1,6 +1,20 @@
+// 同步的 loader
+// module.exports = function (content) {
+//   console.log(content + '001')
+
+//   // 两种方式返回数据
+//   // return content
+//   this.callback(null, content)
+// }
+
+// 异步的 loader  this.async()
 module.exports = function (content) {
   console.log(content + '001')
-  return content + 'dengnwenjie'
+  
+  const callback = this.async()
+  setTimeout(() => {
+    callback(null, content)
+  }, 2000) 
 }
 
 module.exports.pitch = function () {
