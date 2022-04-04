190c451e6226b32be5fd64da32e8cd386fff7947
dengwenj <927561237@qq.com>
2022-04-04 15:20:32 +0800

    vite对ts的支持

--- 02_webpack进阶/14_vite基本使用/src/main.js
+++ 02_webpack进阶/14_vite基本使用/src/main.js
@@ -2,7 +2,10 @@
 import './style/base.css'
 import './style/style.less'
 
+// ts 处理
+import go from './ts'
+
 const message = 'hello vite'
 console.log(message)
 
-
+go('Golang')
---
+++ 02_webpack进阶/14_vite基本使用/src/ts/index.ts
@@ -0,0 +1,3 @@
+export default function go(name: string) {
+  console.log(name)
+}
