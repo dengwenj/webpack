385bd3364cb07f3feb024ddcb08f08f88365af35
dengwenj <927561237@qq.com>
2022-04-04 15:03:58 +0800

    vite的css使用

--- 02_webpack进阶/14_vite基本使用/src/index.js
+++
@@ -1,2 +0,0 @@
-const message = 'hello vite'
-console.log(message)
---
+++ 02_webpack进阶/14_vite基本使用/src/main.js
@@ -0,0 +1,8 @@
+// 对css处理
+import './style/base.css'
+import './style/style.less'
+
+const message = 'hello vite'
+console.log(message)
+
+
